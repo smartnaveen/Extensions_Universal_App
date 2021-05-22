068868146ac7a415772f77d728dd6af82b631eb6
Naveen Kumar <sfs.naveen21@gmail.com>
2021-05-22 23:00:28 +0530

    add UIActivityViewController Files

---
+++ Extension/ActivityViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ActivityViewController.swift
+//  Extension
+//
+//  Created by Mr. Naveen Kumar on 22/05/21.
+//
+
+import Foundation
+import UIKit
+
+// UIActivityViewController - Is a share sheet open in iPhone and iPad, which share text, image, url and anythings media file to with the help of whatsapp, instagram and social meida app.
+
+/*
+ let text = "This is some text that I want to share."
+ let image = UIImage(named: "naveenPhoto.jpg")
+ let myWebsite = URL(string: "https://stackoverflow.com/users/4600136/mr-javed-multani?tab=profile")
+ let shareAll = [text , image! , myWebsite!] as [Any]
+ 
+ let activityViewController = UIActivityViewController(activityItems: shareAll, applicationActivities: nil)
+ activityViewController.completionWithItemsHandler = {
+     (activity, success, items, error) in
+     print("Activity: \(activity) Success: \(success) Items: \(items) Error: \(error)")
+ } // call after send files
+ activityViewController.popoverPresentationController?.sourceView = sender // so that iPads won't crash.
+ 
+// activityViewController.excludedActivityTypes = [ UIActivity.ActivityType.airDrop, UIActivity.ActivityType.postToFacebook ]
+ self.present(activityViewController, animated: true, completion: nil)
+ */
+
+
+
